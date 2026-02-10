dc8972b228fc648b60b129318e1ebfdefaead8f6
Ghazanfar4154 <ghazanfar4154@gmail.com>
2026-02-10 19:58:41 +0500

    Updated

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_PulseStation_FILE C:/Users/GM/Documents/StudioProjects/TLC_projects/pulse_station/build/PulseStation.exe)
-set(__QT_DEPLOY_TARGET_PulseStation_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PulseStation_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
--- build/CMakeFiles/PulseStation.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for PulseStation.
-# This may be replaced when dependencies are built.
